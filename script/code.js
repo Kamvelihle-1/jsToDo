a5bbf18daaeaeaa8b8cc689fd0d348c6e3a1dd81
SmasherK <ksikalan@gmail.com>
2023-06-07 16:17:43 +0200

    editts

--- script/code.js
+++ script/code.js
@@ -1,14 +1,15 @@
-let items = document.querySelector('.display ')
+let items = document.querySelector('.display#disp .col:first-child')
 let input = document.querySelector('#input')
 let order = document.querySelector('#item-sort')
 let add = document.querySelector('#add-item')
 let edit = document.querySelector('#edit')
 let remove = document.querySelector('#remove')
-let check = document.querySelector('#toDo')
-let td=document.querySelector('.item-list')
-let content = document.querySelector('p')
 
-let dataArray =[];
+
+let dataArray =JSON.parse(localStorage.getItem("to-Do-List"))?JSON.parse(localStorage.getItem("to-Do-List")):[];
+let deleteButtons;
+let editButtons;
+let checkButtons;
 let count=[];
 let cnt=0;
 // Add button
@@ -24,37 +25,55 @@ order.addEventListener('click',()=>{
     clear()
     dataArray.sort()
     localStorage.setItem("to-Do-List",JSON.stringify(dataArray))
-    output(dataArray)
+    output()
  })
 
-//  Line through the next
-check.addEventListener('change',()=>{
-    check.setAttribute("aria-checked", check.checked.toString());
-    if (check.ariaChecked) {
-        content.style="text-decoration: line-through";
-        console.log(content.innerText);
-    }
-})
 
 
 // Remove button
-// remove.addEventListener('click',()=>{
-//     console.log(td.id);
-//     let newArray=dataArray.filter((i)=>{
-//         return dataArray.indexOf(i) != td.id
-//     })
-//     console.log(newArray);
-//     td.remove
-//     output(newArray)
-// })
+function deleteItems() {
+    deleteButtons= [...document.querySelectorAll('.close-btn')]
+    deleteButtons.forEach((item)=>{
+        item.addEventListener('click',(event)=>{
+            let sPosition= deleteButton.indexOf(event.target)
+            dataArray.splice(sPosition,1)
+            localStorage.setItem('to-Do-List',dataArray)
+            output();
+        })
+    })
+}
+
+//Check box
+function checkBox() {
+    checkButtons= [...document.querySelectorAll('.todo-checkbox')]
+    checkButtons.forEach((item)=>{
+        item.addEventListener('click',()=>{
+            let iPosition = checkButton.indexOf(event.target)
+            if (dataArray[iPosition].complete === true) {
+                dataArray[iPosition].complete === false;
+            } else {
+                dataArray[iPosition].complete === true
+            }
+            output();
+        })
+    })
+    
+}
+   
 
 // Edit button
-// edit.addEventListener('click',()=>{
-//     console.log(dataArray);
-//     input.value=""
-//     dataArray[eval(td.id)]=input.value
+function editItem() {
+    editButtons = [...document.querySelectorAll('.edit-btn')]
+    editButtons.forEach((item)=>{
+        let newItem= prompt('Enter new item');
+        let i = editButtons.indexOf(event.target);
+        dataArray[i].data = newItem;
+        localStorage.setItem('to-Do-List',JSON.stringify(dataArray))
+        output()
+    })
     
-// })
+}
+
 
 // Validation and code run function
 function addItems(inData) {
@@ -73,13 +92,20 @@ function addItems(inData) {
             
             default:
              
-                dataArray.push(inData.value)
+                dataArray.push({
+                    id:cnt,
+                    data:inData.value,
+                    complete:false,
+                    
+                })
                 console.log(dataArray);
                 count.push(cnt)
+                console.log(dataArray[cnt].id == cnt)
                 cnt++
+               
                 clear()
                 localStorage.setItem("to-Do-List",JSON.stringify(dataArray))
-                output(dataArray)
+                output()
                 inData.value=""
                 break;
     
@@ -91,34 +117,38 @@ function addItems(inData) {
         clear();
     }
     
-    
+    deleteItems();
+    checkBox();
+    editItem();
 }
 
 // Clear function
 function clear() {
     input.value=" "
     items.innerHTML=""
+    
 }
+
 // Display function
-function output(x) {
+function output() {
     let i=0;
-    x=JSON.parse(localStorage.getItem("to-Do-List"));
-    x.forEach(data => {
+    dataArray=JSON.parse(localStorage.getItem("to-Do-List"));
+    dataArray.forEach(data => {
         // td.id=String(count[i]);
         items.innerHTML+=
         `
-        <div class="row my-2 item-list " >
+        <div class="row my-2 item-list text-center " id="${data.id}" >
             <div class="col-1 ">
-                <input type="checkbox" name="toDo" id="toDo" aria-checked="false">
+            <input type="checkbox"  name="toDo" id="toDo" class="todo-checkbox aria-checked="false">
             </div>
             <div class="col-1">
-                <p>${data}</p>
+                <p>${data.data}</p>
             </div>
             <div class="col-1">
-                <button id="edit">Edit</button>
+                <button id="edit" class="edit-btn">Edit</button>
             </div>
             <div class="col-1">
-                <button id="remove" >X</button>
+            <button id="remove" class="close-btn" >X</button>
             </div>
                            
         </div>
